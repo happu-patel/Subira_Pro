ac2686f4f4015dbd0d095f76c2e6a38a027aa2ee
Patel happy <131694901+happu-patel@users.noreply.github.com>
2024-08-03 13:15:52 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import '../../App.css';
 import './header.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,14 +8,28 @@ import Logo from '../../assets/images/Mask group.png';
 import PlayCircle from '../../assets/images/play-circle.png';
 import MacBook from '../../assets/images/MacBook Pro 16.png';
 import MacBookContent from '../../assets/images/Image1.png';
-const Header = () => {
+const Header = ({ scrollToPricing, scrollToFeature, scrollToServices, scrollToFaq }) => {
+    const handlePricingClick = () => {
+        scrollToPricing();
+    };
+
+    const handleFeatureClick = () => {
+        scrollToFeature();
+    }
+
+    const handleServicesClick = () => {
+        scrollToServices();
+    }
+
+    const handleFaqClick = () => {
+        scrollToFaq();
+    }
 
     return (
         <>
-            {/* Hello world */}
             <nav className="navbar navbar-expand-lg align-items-center">
                 <div className="container">
-                    <a className="navbar-brand" href="#">
+                    <a className="navbar-brand" href="/">
                         <img src={Logo} width={'106px'} alt='logo' />
                     </a>
                     <button
@@ -28,63 +43,35 @@ const Header = () => {
                     >
                         <span className="navbar-toggler-icon" />
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                        <ul className="navbar-nav  mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <Link className="nav-link" aria-current="page" to="/">
                                     Home
                                 </Link>
                             </li>
+
                             <li className="nav-item">
-                                <Link className="nav-link" to="/features">
-                                    Products
-                                </Link>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <Link
-                                    className="nav-link dropdown-toggle"
-                                    to="/resouces"
-                                    role="button"
-                                    data-bs-toggle="dropdown"
-                                    aria-expanded="false"
-                                >
-                                    Resources
-                                </Link>
-                                <ul className="dropdown-menu">
-                                    <li>
-                                        <a className="dropdown-item" href="/action">
-                                            Action
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a className="dropdown-item" href="/anothe">
-                                            Another action
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <hr className="dropdown-divider" />
-                                    </li>
-                                    <li>
-                                        <a className="dropdown-item" href="/something">
-                                            Something else here
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" aria-disabled="true" to='/about'>
+                                <button className="nav-link" onClick={handlePricingClick}>
                                     Pricing
-                                </Link>
+                                </button>
+                            </li>
+                            <li className="nav-item">
+                                <button className="nav-link" onClick={handleFeatureClick}>
+                                    Features
+                                </button>
+                            </li>
+                            <li className="nav-item">
+                                <button className="nav-link" onClick={handleServicesClick}>
+                                    Services
+                                </button>
+                            </li>
+                            <li className="nav-item">
+                                <button className="nav-link" onClick={handleFaqClick}>
+                                    Faq
+                                </button>
                             </li>
                         </ul>
-                        <div className='nav-btn'>
-                            <button className="btn btn-login" type="submit">
-                                Log in
-                            </button>
-                            <button className="btn btn-primary" type="submit">
-                                Sign up
-                            </button>
-                        </div>
                     </div>
                 </div>
             </nav>
@@ -92,17 +79,17 @@ const Header = () => {
                 <div className='container'>
                     <div className='header-content'>
                         <h1>Bring your business workflow to life</h1>
-                        <p>Expedite your workflow  with Same-day deal signing and payment.</p>
-                        <p>Create and issue online invoices, allowing recipients to pay immediately. Subirapro  is an easy and <br /> affordable solution for small and medium businesses to accept payments.</p>
+                        <p>Expedite your workflow with Same-day deal signing and payment.</p>
+                        <p>Create and issue online invoices, allowing recipients to pay immediately. Subirapro is an easy and <br /> affordable solution for small and medium businesses to accept payments.</p>
                     </div>
                     <div className='header_buttons'>
-                        <a href='demo'>
+                        <a href='/'>
                             <button className='demo-btn'>
                                 <img src={PlayCircle} alt='playicon' className='d-icon' />
                                 <span>Request Demo</span>
                             </button>
                         </a>
-                        <a href='signup'>
+                        <a href='/'>
                             <button className="sign-btn" type="submit">
                                 Sign up
                             </button>
@@ -119,7 +106,6 @@ const Header = () => {
                 </div>
             </header>
         </>
-
     );
 };
 
