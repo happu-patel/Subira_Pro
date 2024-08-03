c9192c9ec36e44dedeaff2f980df89402db48efa
Patel happy <131694901+happu-patel@users.noreply.github.com>
2024-08-03 14:56:25 +0530

    Update Model.jsx

--- src/components/Faq/Model.jsx
+++ src/components/Faq/Model.jsx
@@ -20,43 +20,7 @@ const Modal = ({ showModal, closeModal }) => {
             <p className='supporting_text'>We’d love to hear from you. Please fill out this form.</p>
           </div>
       
-          <div className="modal-body">
-            <form>
-              <div className='row'>
-                <div className='col-lg-6 col-md-6 col-sm-12'>
-                  <label>First name</label><br />
-                  <input type='text' placeholder='First name' />
-                </div>
-                <div className='col-lg-6 col-md-6 col-sm-12'>
-                  <label>Last name</label><br />
-                  <input type='text' placeholder='Last name' />
-                </div>
-                <div className='col-lg-12 col-md-12 col-sm-12'>
-                  <label>Email</label><br />
-                  <input type='email' placeholder='you@company.com' className='mail' />
-                </div>
-                <div className='col-lg-12 col-md-12 col-sm-12 phone'>
-                  <label>Phone number</label><br />
-                  <div className='input-group'>
-                    <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Us</button>
-                    <ul className="dropdown-menu">
-                      <li><a className="dropdown-item" href="/action">India</a></li>
-                      <li><a className="dropdown-item" href="/another action">UK</a></li>
-                      <li><a className="dropdown-item" href="/something">Ny</a></li>
-                    </ul>
-                    <input type="text" aria-label="Text input with dropdown button" placeholder='+1 (555) 000-0000' />
-                  </div>
-                </div>
-                <div className='col-lg-12 col-md-12 col-sm-12'>
-                  <label>Message</label><br />
-                  <textarea></textarea>
-                </div>
-                <div className='submit-btn'>
-                  <button type='submit'>Send message</button>
-                </div>
-              </div>
-            </form>
-          </div>
+          
         </div>
       </div>
     </div>
