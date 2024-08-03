f4afcc77c6fe6cc368cd4aa79f1712951dae6a15
Patel happy <131694901+happu-patel@users.noreply.github.com>
2024-08-03 13:14:52 +0530

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,55 @@
-import React from 'react'
-import Features from '../../components/Features/Features'
-import Faq from '../../components/Faq/Faq'
-import Pricing from '../../components/Pricing/Pricing'
+import React, { useRef, useEffect } from 'react';
+import Features from '../../components/Features/Features';
+import Faq from '../../components/Faq/Faq';
+import Pricing from '../../components/Pricing/Pricing';
+import Services from '../../components/Services/Services';
 
-const Home = () => {
+const Home = ({ scrollToPricing, scrollToFeature, scrollToServices, scrollToFaq }) => {
+  const pricingRef = useRef(null);
+  const featureRef = useRef(null);
+  const serviceRef = useRef(null);
+  const faqRef = useRef(null);
+
+  useEffect(() => {
+    if (scrollToPricing) {
+      pricingRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [scrollToPricing]);
+
+  useEffect(() => {
+    if (scrollToFeature) {
+      featureRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [scrollToFeature]);
+
+  useEffect(() => {
+    if (scrollToServices) {
+      serviceRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }
+  })
+
+  useEffect(() => {
+    if (scrollToFaq) {
+      faqRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }
+  })
+
   return (
     <>
-    <Features/>
-    <Faq/>
-    <Pricing/>
+      <div ref={featureRef}>
+        <Features />
+      </div>
+      <div ref={serviceRef}>
+        <Services />
+      </div>
+      <div ref={faqRef}>
+        <Faq />
+      </div>
+      <div ref={pricingRef}>
+        <Pricing />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
